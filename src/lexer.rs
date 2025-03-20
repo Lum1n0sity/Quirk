e5db70d74d7536da6db2de491c63c29adc6ed911
Raphael Schuhmann <raphael221@outlook.de>
2025-03-20 10:19:51 +0100

    Added Token struct, Added basic tokenize function in lexer.rs with no tokenization logic

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,10 @@
 #![allow(dead_code)]
 #![allow(redundant_semicolons)]
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 pub enum TokenTypes {
     // Keywords
     KeywordFn,
@@ -64,4 +68,31 @@ pub enum TokenTypes {
     CommentBlock,
     EOF,
     Unknown,
+}
+
+pub struct Token {
+    pub token_type: TokenTypes,
+    pub value: String,
+    pub line: u32,
+    pub column: u32
+}
+
+pub fn tokenize(file_path: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+
+    let path = Path::new(file_path);
+    let mut file = File::open(&path).expect("Error: Could not open file: {}", file_path);
+    let mut content = String::new();
+    file.read_to_string(&mut content).expect("Error: Could not read file: {}", file_path);
+
+    // Tokenization logic
+
+    tokens.push(Token {
+        token_type: TokenTypes::EOF,
+        value: "".to_string(),
+        line: 0,
+        column: 0
+    });
+
+    tokens
 }
