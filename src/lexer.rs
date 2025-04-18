a7be14921256d0d7bc416df272293c083b7d3604
Raphael Schuhmann <raphael221@outlook.de>
2025-04-18 14:35:01 +0200

    Removed KeywordNew since it isn't needed anymore

--- src/lexer.rs
+++ src/lexer.rs
@@ -14,7 +14,7 @@ pub enum TokenType {
     KeywordFn, KeywordImmut, KeywordLet, KeywordHeap, KeywordFree, KeywordReturn,
     KeywordIf, KeywordElse, KeywordElIf, KeywordFor, KeywordWhile, KeywordBreak,
     KeywordContinue, KeywordOut,  KeywordEnum, KeywordStruct,
-    KeywordClass, KeywordConstruct,  KeywordThis, KeywordNew, KeywordImport, KeywordAsync, KeywordAwait,
+    KeywordClass, KeywordConstruct,  KeywordThis, KeywordImport, KeywordAsync, KeywordAwait,
     // Data Types
     DataTypeInt, DataTypeUint, DataTypeInt8, DataTypeUint8, DataTypeInt16, DataTypeUint16,
     DataTypeInt32, DataTypeUint32, DataTypeInt64, DataTypeUint64, DataTypeInt128, DataTypeUint128,
@@ -306,7 +306,6 @@ fn match_keyword(word: &str) -> Option<TokenType> {
         "class" => Some(TokenType::KeywordClass),
         "struct" => Some(TokenType::KeywordStruct),
         "out" => Some(TokenType::KeywordOut),
-        "new" => Some(TokenType::KeywordNew),
         "this" => Some(TokenType::KeywordThis),
         // Data Types
         "int" => Some(TokenType::DataTypeInt),
